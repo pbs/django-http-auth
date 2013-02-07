94cf69ccada52c3a58f3f0e4145d906993300769
Dimulescu Petru <petru.dimulescu@3pillarglobal.com>
2013-02-07 18:38:42 +0200

    LUN-416. Removed all auth when accessing pages from admin.

--- multisiteauth/middleware.py
+++ multisiteauth/middleware.py
@@ -1,7 +1,8 @@
-from django.conf import settings
-from django.http import HttpResponse, get_host
-from django.core.urlresolvers import resolve
+from django.http import HttpResponse
+from django.core.urlresolvers import resolve, reverse
 from django.contrib.sites.models import Site
+from django.core.exceptions import MiddlewareNotUsed
+from multisiteauth import settings as local_settings
 
 import base64
 import logging
@@ -11,25 +12,22 @@ class BasicAuthProtectionMiddleware(object):
     Some middleware to authenticate requests.
     """
     def __init__(self):
-        self.basic_auth_enabled = getattr(settings, 'BASIC_HTTP_AUTH_ENABLED', False)
-        self.basic_auth_username = getattr(settings, 'BASIC_HTTP_AUTH_GENERAL_USERNAME', '')
-        self.basic_auth_password = getattr(settings, 'BASIC_HTTP_AUTH_GENERAL_PASS', '')
+        if not local_settings.HTTP_AUTH_ENABLED:
+            raise MiddlewareNotUsed("Basic authentication is not used, this removes it from middleware")
         # if looking only for blocking access for bad-behaved crawlers SSL is not required
         # BEWARE: without encryption the basic auth credentials are sent in plain text
         #self.basic_auth_requires_ssl = getattr(settings, 'BASIC_HTTP_AUTH_USE_SSL', '')
-        self.whitelisted_views = set(getattr(settings, 'BASIC_HTTP_AUTH_BYPASSED_VIEWS', []))
-        self.allow_admin = getattr(settings, 'BASIC_HTTP_AUTH_ALLOW_ADMIN', True)
 
     def process_request(self, request):
         # adapted from https://github.com/amrox/django-moat/blob/master/moat/middleware.py
         # check if it's globally disabled
-        if self.basic_auth_enabled:
+        if local_settings.HTTP_AUTH_ENABLED:
             current_site = Site.objects.get_current()
             if hasattr(current_site, 'siteauthorizationstatus'):
                 auth_status = getattr(current_site , 'siteauthorizationstatus', None)
                 if auth_status and auth_status.require_basic_authentication:
                     # check if we are already authenticated
-                    if request.session.get('basicauth_username') != None:
+                    if request.session.get('basicauth_username'):
                         logging.info("Already authenticated as: " + request.session.get('basicauth_username'))
                         return None
                     else:
@@ -39,11 +37,10 @@ class BasicAuthProtectionMiddleware(object):
                     full_view_name = '%s.%s' % (view_func.__module__, view_func.__name__)
                     #if our view needs to bypass auth, then we just continue.
                     # if it's not whitelisted we need to ask for credentials
-                    if full_view_name in self.whitelisted_views:
+                    if full_view_name in local_settings.HTTP_AUTH_BYPASSED_VIEWS:
                         return None
-                    if self.allow_admin and\
-                       (view_func.__module__.startswith('django.contrib.admin') or\
-                        view_func.__module__.startswith('cms.admin')):
+
+                    if local_settings.HTTP_AUTH_ALLOW_ADMIN and request.path.startswith(reverse('admin:index')):
                         return None
 
                     # Check for "cloud" HTTPS environments
@@ -65,17 +62,18 @@ class BasicAuthProtectionMiddleware(object):
                 if auth[0].lower() == 'basic':
                     # Currently, only basic http auth is used.
                     uname, passwd = base64.b64decode(auth[1]).split(':')
-                    if uname==self.basic_auth_username and passwd == self.basic_auth_password:
+                    if uname==local_settings.HTTP_AUTH_GENERAL_USERNAME and \
+                       passwd == local_settings.HTTP_AUTH_GENERAL_PASS:
                         request.session['basicauth_username'] = uname
                         return None
 
-        # The username/password combo was incorrect, or not provided.
+        # The username/password combo was incorrect, or not logprovided.
         # Challenge the user for a username/password.
         resp = HttpResponse()
         resp.status_code = 401
         try:
             # If we have a realm in our settings, use this for the challenge.
-            realm = settings.HTTP_AUTH_REALM
+            realm = local_settings.HTTP_AUTH_REALM
         except AttributeError:
             realm = ""
 
