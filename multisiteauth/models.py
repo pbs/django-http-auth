531e1185767fba95ea258dfecdd73450c7a835e6
Dimulescu Petru <petru.dimulescu@3pillarglobal.com>
2013-02-20 11:48:35 +0200

    LUN-261. Changed verbose name according to Markham's remark.

--- multisiteauth/models.py
+++ multisiteauth/models.py
@@ -4,7 +4,7 @@ from django.contrib.sites.models import Site
 
 class SiteAuthorizationStatus(models.Model):
     site = models.OneToOneField(Site)
-    require_basic_authentication = models.BooleanField(verbose_name="Require basic HTTP authorization",
+    require_basic_authentication = models.BooleanField(verbose_name="Check to add password protection to this site.",
                                                        default=False,
                                                        blank=True
     )
