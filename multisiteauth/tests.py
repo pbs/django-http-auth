dc88981ea579bf11b960c22a00bf9e5d4e340ce3
Dimulescu Petru <petru.dimulescu@3pillarglobal.com>
2013-02-25 16:06:50 +0200

    LUN-455. Updated tests to run in a different way.

--- multisiteauth/tests.py
+++ multisiteauth/tests.py
@@ -8,62 +8,100 @@ from django.test.client import Client
 from multisiteauth import settings as local_settings
 
 
-class CheckAuthorizationTestCase(TestCase):
+def _auth_encode(username, password):
+    """helper method to generate auth encoding"""
+    return "Basic " + base64.b64encode("%s:%s" % (username, password))
+
+
+class CheckSiteRequiresAuthTestCase(TestCase):
     fixtures = ['siteauth.json']
 
     def setUp(self):
         self.client = Client(HTTP_HOST="test-site1.local.org")
-        self.clientTwo = Client(HTTP_HOST="test-site2.local.org")
         local_settings.HTTP_AUTH_ENABLED = True
         local_settings.HTTP_AUTH_GENERAL_USERNAME = 'basic_http_user'
         local_settings.HTTP_AUTH_GENERAL_PASS = "basic_http_pass"
         local_settings.HTTP_AUTH_REALM = ''
-        self.adminuser = User.objects.create_superuser(username="user", email="test@test.org", password="password")
-        self.adminuser.save()
+        self.settings(SITE_ID=1)
 
 
     def tearDown(self):
         self.client = None
-        self.clientTwo = None
-        self.adminuser.delete()
-
-    def _auth_encode(self, username, password):
-        """helper method to generate auth encoding"""
-        return "Basic " + base64.b64encode("%s:%s" % (username, password))
-
 
     def testAuthRequest(self):
         response = self.client.get('/', {})
         self.assertEquals(response.status_code, 401, "Authorization was not requested for Site 1")
-        response = self.clientTwo.get('/', {})
-        self.assertNotEqual(response.status_code, 401, "Authorization was requested for Site 2")
+
 
     def testAuthLogin(self):
-        response = self.client.get('/', **{'HTTP_AUTHORIZATION': self._auth_encode("basuser", "badpass")})
-        self.assertEqual(response.status_code, 401, "Authorized to access Site 1 with wrong credentials")
+        response = self.client.get('/', **{'HTTP_AUTHORIZATION': _auth_encode("baduser", "badpass")})
+        self.assertEqual(response.status_code, 401,
+                         "Response status code: %s\nAuthorized to access Site 1 with wrong credentials" \
+                         %response.status_code)
         response = self.client.get('/',
-                                   **{'HTTP_AUTHORIZATION': self._auth_encode(local_settings.HTTP_AUTH_GENERAL_USERNAME,
-                                                                              local_settings.HTTP_AUTH_GENERAL_PASS)})
-        self.assertNotEqual(response.status_code, 401, "Not authorized to access Site 1 pages for correct credentials")
+                                   **{'HTTP_AUTHORIZATION': _auth_encode(local_settings.HTTP_AUTH_GENERAL_USERNAME,
+                                                                         local_settings.HTTP_AUTH_GENERAL_PASS)})
+        self.assertNotEqual(response.status_code, 401,
+                            "Response status code: %s\nNot authorized to access Site 1 pages for correct credentials" \
+                            %response.status_code)
+
+
+class AdminAuthorizationSiteTestCase(TestCase):
+    fixtures = ['siteauth.json']
+
+    def setUp(self):
+        self.client = Client(HTTP_HOST="test-site1.local.org")
+        local_settings.HTTP_AUTH_ENABLED = True
+        local_settings.HTTP_AUTH_GENERAL_USERNAME = 'basic_http_user'
+        local_settings.HTTP_AUTH_GENERAL_PASS = "basic_http_pass"
+        local_settings.HTTP_AUTH_REALM = ''
+        self.settings(SITE_ID=1)
+        superuser = User.objects.create_superuser(username="user", email="test@test.org", password="password")
+        superuser.save()
+
+
+    def tearDown(self):
+        self.client = None
 
     def testBypassAdminAuthorizedClients(self):
-        allow_admin = local_settings.HTTP_AUTH_ALLOW_ADMIN
-        local_settings.HTTP_AUTH_ALLOW_ADMIN = True
-        if self.client.login(username="user", password="password"):
-            response = self.client.get('/', {})
-            self.assertNotEquals(response.status_code, 401, "Authorization was requested for authenticated user.")
-            self.client.logout()
-            response = self.client.get('/', {})
-            self.assertEquals(response.status_code, 401, "Authorization was not requested for Anonymous user.")
-        else:
-            self.assertTrue(False, "Check the fixture. Could not login to admin with 'user:password' dummy credentials")
-        local_settings.HTTP_AUTH_ALLOW_ADMIN = allow_admin
+        with self.settings(HTTP_AUTH_ALLOW_ADMIN=True):
+            if self.client.login(username="user", password="password"):
+                response = self.client.get('/', {})
+                self.assertNotEquals(response.status_code, 401, "Authorization was requested for authenticated user.")
+                self.client.logout()
+                response = self.client.get('/', {})
+                self.assertEquals(response.status_code, 401, "Authorization was not requested for Anonymous user.")
+            else:
+                self.assertTrue(False,
+                                "Check the fixture. Could not login to admin with 'user:password' dummy credentials")
 
     def testNoBypassAdminAuthorizedClients(self):
-        allow_admin = local_settings.HTTP_AUTH_ALLOW_ADMIN
-        local_settings.HTTP_AUTH_ALLOW_ADMIN = False
-        response = self.client.get(reverse('admin:index'), {})
-        self.assertEquals(response.status_code, 401, "Authorization was not requested for admin (admin bypass off).")
-        local_settings.HTTP_AUTH_ALLOW_ADMIN = allow_admin
+        with self.settings(HTTP_AUTH_ALLOW_ADMIN=False):
+            allow_admin = local_settings.HTTP_AUTH_ALLOW_ADMIN
+            local_settings.HTTP_AUTH_ALLOW_ADMIN = False
+            response = self.client.get(reverse('admin:index'), {})
+            self.assertEquals(response.status_code, 401,
+                              "Authorization was not requested for admin (admin bypass off).")
+            local_settings.HTTP_AUTH_ALLOW_ADMIN = allow_admin
+
+
+class CheckUnauthorizedSiteTestCase(TestCase):
+    fixtures = ['siteauth.json']
+
+    def setUp(self):
+        self.client = Client(HTTP_HOST="test-site2.local.org")
+        local_settings.HTTP_AUTH_ENABLED = True
+        local_settings.HTTP_AUTH_GENERAL_USERNAME = 'basic_http_user'
+        local_settings.HTTP_AUTH_GENERAL_PASS = "basic_http_pass"
+        local_settings.HTTP_AUTH_REALM = ''
+        self.settings(SITE_ID=2)
+
+
+    def tearDown(self):
+        self.client = None
+
+    def testAuthRequest(self):
+        response = self.client.get('/', {})
+        self.assertNotEqual(response.status_code, 401, "Authorization was requested for Site 2")
 
 
