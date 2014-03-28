5d7208184f7c88c0e864fa3a047fcc560ec29614
Andrei Horak <linkyndy@gmail.com>
2014-03-28 12:15:49 +0200

    (tox) Changed test database to :memory:

--- multisiteauth/tests/settings.py
+++ multisiteauth/tests/settings.py
@@ -27,11 +27,7 @@ TEMPLATE_CONTEXT_PROCESSORS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME' : 'test.db', # Or path to database file if using sqlite3.
-        'USER' : '', # Not used with sqlite3.
-        'PASSWORD' : '', # Not used with sqlite3.
-        'HOST' : '', # Set to empty string for localhost. Not used with sqlite3.
-        'PORT' : '', # Set to empty string for default. Not used with sqlite3.
+        'NAME' : ':memory:',
     }
 }
 MIDDLEWARE_CLASSES = (
