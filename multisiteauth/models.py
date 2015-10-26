6887b2be589cdf1dd2878309957c4fa8672cd3d7
bogdan.virtosu <virtosu.bogdan@gmail.com>
2015-10-26 16:55:31 +0200

    LUN-2737 Small improvements in UI/code.

--- multisiteauth/models.py
+++ multisiteauth/models.py
@@ -4,7 +4,12 @@ from django.contrib.sites.models import Site
 
 class SiteAuthorizationStatus(models.Model):
     site = models.OneToOneField(Site)
-    require_basic_authentication = models.BooleanField(verbose_name="Check to add password protection to this site.",
-                                                       default=False,
-                                                       blank=True
+    require_basic_authentication = models.BooleanField(
+        verbose_name="Check to add password protection to this site.",
+        default=False,
+        blank=True
     )
+
+    class Meta:
+        verbose_name = "Site Authorization Status"
+        verbose_name_plural = verbose_name
