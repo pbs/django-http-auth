933d26763b52819e63e80538833a8bce3fb871a4
bogdan.virtosu <virtosu.bogdan@gmail.com>
2016-10-24 11:44:08 +0300

    Bump version to 1.1.1.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ dependencies = ["Django>=1.4,<1.9"]
 
 setup(
     name='django-http-auth',
-    version='1.1.0',
+    version='1.1.1',
     description='Simple django multisite Basic HTTP authentication middleware',
     author='TPG Bento Team',
     author_email='TPG-PBS-Bento@3pillarglobal.com',
